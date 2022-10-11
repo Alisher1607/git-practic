838243a690f926adac49112d6bf4df3fb6e7aad8
Alisher <zumanovaliser814@gmail.com>
2022-10-11 20:19:36 +0600

    practic onlain

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Hello</h1>
+      <h1>World</h1>
+      <h1>Welcome</h1>
+      <h1>To</h1>
+      <h1>Peaksoft</h1>
     </div>
   );
 }
